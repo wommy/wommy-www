85cd46c9524703c1ef3a41dd0a010139fb46313c
wommy <tom@technomad.media>
2022-02-08 23:19:58 -0500

    just gotta move assets

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,12 @@
 module.exports = function (c) {
+	c.ignores.add('src/_content')
 	c.addPassthroughCopy({ "_static/*": "assets/" })
 	return {
 		htmlTemplateEngine: 'njk',
 		markdownTemplateEngine: 'njk',
 		dir: {
 			input: 'src',
+			layouts: '_layouts',
 		}
 	}
 }
---
+++ slinkity.config.js
@@ -0,0 +1,6 @@
+import { defineConfig } from 'slinkity'
+import reactRenderer from '@slinkity/renderer-react'
+
+export default defineConfig({
+  renderers: [reactRenderer],
+})
