c648d00da60afbac012d6d0d85696e8f91a92005
wommy <tom@technomad.media>
2022-02-09 15:03:54 -0500

    init: config

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'wommy',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'About', link: '/about' },
+      { text: 'Clients', link: '/clients' },
+    ],
+    sidebar: [
+      { text: 'Brands', link: '/about/brands' },
+      { text: 'Resume', link: '/about/resume' },
+    ],
+  },
+}
