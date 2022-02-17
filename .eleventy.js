4e56fa1be57cd5d38c2faa5debef94abb6188a4f
wommy <tom@technomad.media>
2022-02-16 20:32:01 -0500

    feat: .config.input: 'src'

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,4 @@
 module.exports = function (c) {
 	c.addPassthroughCopy({ "_static": "assets" })
-	return { htmlTemplateEngine: 'njk', }
+	return { htmlTemplateEngine: 'njk', dir: { input: 'src' }}
 }
