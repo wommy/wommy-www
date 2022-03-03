25cfa6ba6954f3bbb06ce79ddd5e0931918c80b4
wommy <tom@technomad.media>
2022-03-03 18:22:03 -0500

    FEAT: JSDoc hinting

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,11 @@
-module.exports = function (c) {
-	c.addPassthroughCopy("assets")
-	c.addPassthroughCopy("favicon.ico")
-	return { htmlTemplateEngine: 'njk', }
+/** https://github.com/11ty/eleventy/discussions/2089
+ * @param {import('@11ty/eleventy/src/UserConfig')} eleventyConfig
+ * @returns {ReturnType<import('@11ty/eleventy/src/defaultConfig')>}
+ */
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPassthroughCopy('assets')
+  return {
+    htmlTemplateEngine: 'njk',
+    dir: { input: 'src' },
+  }
 }
