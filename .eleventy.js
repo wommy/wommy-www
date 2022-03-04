457becb152ac5e565491a2c27b0c3d08c0106d63
wommy <tom@technomad.media>
2022-03-03 22:43:03 -0500

    _site to dist, _content to content

--- .eleventy.js
+++ .eleventy.js
@@ -10,6 +10,9 @@ module.exports = function (eleventyConfig) {
     })
   return {
     htmlTemplateEngine: 'njk',
-    dir: { input: 'src' },
+    dir: { 
+      input: 'src',
+      output: 'dist',
+    },
   }
 }
